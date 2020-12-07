34b58d04c807898e837635825ea8682e4d082212
Michael Dinerstein <michael@dtweb.design>
2020-12-07 04:54:30 +0000

    Added shell script for quick bulk renaming

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+declare -A RENAME
+
+# Rename all "example-me" below to your own plugin name.
+# Be sure to leave all array key indexes named as-is!
+RENAME[plugin-name]="example-me"
+RENAME[plugin_name]="example_me"
+RENAME[Plugin_Name]="Example_Me"
+RENAME[PLUGIN_NAME_]="EXAMPLE_ME_"
+RENAME[WordPress Plugin Boilerplate webpack]="Example Me"
+
+###
+# Loops over RENAME array and search/replace for the key/value
+for K in "${!RENAME[@]}"; do
+    git grep -l "$K" | xargs sed -i "s/$K/${RENAME[$K]}/g"
+done
+
+# Find and rename all files with "plugin-name" in the filename to the value specificed in RENAME[plugin-name]
+find . -maxdepth 4 -type f -name "*plugin-name*.*" | while read FNAME; do mv "$FNAME" "${FNAME//plugin-name/${RENAME[plugin-name]}}"; done
