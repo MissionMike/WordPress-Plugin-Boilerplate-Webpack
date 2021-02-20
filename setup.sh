711c4243c5f88bf7437b96dde0834190287920f3
Michael Dinerstein <michael@dtweb.design>
2021-02-19 22:25:37 -0800

    Add a comment

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 declare -A RENAME
 
 # Rename all "example-me" below to your own plugin name.
-# Be sure to leave all array key indexes named as-is!
+# Be sure to leave all array key indexes named as-is!!
 RENAME[plugin-name]="example-me"
 RENAME[plugin_name]="example_me"
 RENAME[Plugin_Name]="Example_Me"
@@ -10,7 +10,8 @@ RENAME[PLUGIN_NAME_]="EXAMPLE_ME_"
 RENAME[WordPress Plugin Boilerplate webpack]="Example Me"
 
 ###
-# Loops over RENAME array and search/replace for the key/value
+# Loops over RENAME array and search/replaces the key for the value
+# Example: plugin-name --> example-me
 for K in "${!RENAME[@]}"; do
     git grep -l "$K" | xargs sed -i "s/$K/${RENAME[$K]}/g"
 done
